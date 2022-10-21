8c51fd2018ae0fe7be03be6bb0076bbf27fa39af
Temur Vegas <timanaimoff@gmail.com>
2022-10-21 22:20:00 +0500

    Added webmvc functional(Spring) and jstl-libraries

--- src/main/java/edu/selfstudy/ext/rest/StudentController.java
+++ src/main/java/edu/selfstudy/ext/rest/StudentController.java
@@ -7,23 +7,28 @@ import edu.selfstudy.ext.view.StudentRequest;
 import edu.selfstudy.ext.view.StudentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Component
-@Path("/student")
+@RestController
+@RequestMapping(path="/student")
 public class StudentController {
     @Autowired
     private StudentService studentService;
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+
     public List<StudentResponse> getStudentInfo(StudentRequest studentRequest){
          return studentService.getStudentInfo(studentRequest);
+    }
+    @GetMapping(path="/check")
+    public String checkAdmin(){
+        return "Rest Service is working!";
     }
 }
